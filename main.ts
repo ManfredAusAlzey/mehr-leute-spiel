e22c4f897c5cf58a192ba23789d8564826637087
ManfredAusAlzey <142033758+ManfredAusAlzey@users.noreply.github.com>
2023-08-12 12:01:17 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,72 @@
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 8 8 f f f . . . . 
+    . . . f f f 8 8 8 8 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 8 8 8 8 8 8 e e f . . 
+    . . f e 8 f f f f f f 8 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f e e 6 6 e e f e f f . 
+    . f e e 6 1 f d d f 1 6 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 6 6 6 6 e e f . . . 
+    . . e 6 f 8 8 8 8 8 8 f 6 e . . 
+    . . 6 d f 8 8 8 8 8 8 f d 6 . . 
+    . . 6 6 f 6 6 5 5 6 6 f 6 6 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 4 4 f f f . . . . 
+    . . . f f f 4 4 4 4 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 4 4 4 4 4 4 e e f . . 
+    . . f e 4 f f f f f f 4 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f e e 4 4 e e f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 4 4 4 4 4 4 f 4 e . . 
+    . . 4 d f 4 4 4 4 4 4 f d 4 . . 
+    . . 4 d f 2 2 5 5 2 2 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 7 7 f f f . . . . 
+    . . . f f f 7 7 7 7 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f e e 7 7 7 7 7 7 e e f . . 
+    . . f e 7 f f f f f f 7 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f e e 7 7 e e f e f f . 
+    . f e e 7 1 f d d f 1 7 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 7 7 7 7 e e f . . . 
+    . . e 7 f 7 7 7 7 7 7 f 7 e . . 
+    . . 7 d f 7 7 7 7 7 7 f d 7 . . 
+    . . 7 7 f 2 2 5 5 2 2 f 7 7 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player))
